bd4a0416ac8f510a9c1b6da99a0ddf1f1dd3fd10
ahmedaboutlaeb <ahmedaboutaleb94@gmail.com>
2026-03-26 09:34:48 +0200

    adding Bonus

--- bonus.js
+++ bonus.js
@@ -1,1 +1,19 @@
 let x;
+function findKthPositive(arr, k) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    let missing = arr[mid] - (mid + 1);
+
+    if (missing < k) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return left + k;
+}
